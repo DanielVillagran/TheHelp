5f32316400f992dc120c143b3ba0c7411fd11ee5
Oswaldo Daniel Villagrana Mora <ovillagrana@ho1a.com>
2022-01-18 22:17:57 -0600

    Cambios

--- assets/js/autoload/forms/Servicios.js
+++ assets/js/autoload/forms/Servicios.js
@@ -103,7 +103,7 @@ function readURL(input) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            $('#icon-preview').attr('src', e.target.result);
+            $(".current_logo").html('<img src="/assets/' + e.target.result + '"/>');
         }
 
         reader.readAsDataURL(input.files[0]);
